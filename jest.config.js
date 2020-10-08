bb54c5a051155fbbb1a0d446b2db0ce903aa98d5
wtlin <wtlin1228@gmail.com>
2020-10-08 18:32:50 +0800

    test(testing): add jest and testing-library

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// eslint-ignore-next-line
+module.exports = {
+  moduleDirectories: ['node_modules', './src'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+}
---
+++ src/packages/layout/__tests__/dashboard.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import StudentLayout from '../components/StudentLayout'
+
+describe('StudentLayout', () => {
+  it('renders without crashing', () => {
+    render(<StudentLayout />)
+    expect(screen.getByRole('img', { name: 'logo' })).toBeInTheDocument()
+  })
+})
