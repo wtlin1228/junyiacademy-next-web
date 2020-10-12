3a1cc542875f4c612b7af7150f3a4ce3fef2bd82
wtlin <wtlin1228@gmail.com>
2020-10-12 11:47:46 +0800

    chore(coverage): add code coverage setting

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,24 @@
-// eslint-ignore-next-line
 module.exports = {
   moduleDirectories: ['node_modules', './src'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/tests/**',
+    '!**/coverage/**',
+    '!**/.next/**',
+    '!jest.config.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
   },
 }
